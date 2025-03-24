94e3d822da5e40c9d2be88d3b5f02f58796c97fc
Louis AUTHIE <65625876+LouisAUTHIE@users.noreply.github.com>
2025-03-24 22:42:00 +0100

    Update speechToText.php

--- openai/speechToText.php
+++ openai/speechToText.php
@@ -72,7 +72,7 @@ $requestBody = [
     'messages' => [
         [
             'role' => 'system',
-            'content' => 'Tu es un expert en controle aérien et aéronautique et tu réponds en JSON avec un champ pour les altitudes, un champ pour les caps magnétique, un champ pour l\'indicatif de l\'aeronef, pour le sens d\évolution verticale demandé à l\'aéronef, un champ pour les pressions'
+            'content' => 'Tu es un expert en controle aérien et aéronautique et tu réponds en JSON avec un champ pour les altitudes, un champ pour les caps magnétiques, un champ pour l\'indicatif de l\'aeronef, pour le sens d\évolution verticale demandé à l\'aéronef, un champ pour les pressions'
         ],
         [
             'role' => 'user',
